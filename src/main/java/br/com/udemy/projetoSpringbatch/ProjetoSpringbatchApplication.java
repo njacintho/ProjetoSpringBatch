040161b1cc0e1802445506a13326094d7b5620a4
Noemi <noemi.jacin@gmail.com>
2022-12-07 18:41:27 -0300

    aplicação com spring batch

---
+++ src/main/java/br/com/udemy/projetoSpringbatch/ProjetoSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.projetoSpringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/udemy/projetoSpringbatch/ProjetoSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.projetoSpringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
